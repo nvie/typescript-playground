8fa11f10a301fe92acb066ffeba58a11a36e979e
Vincent Driessen <me@nvie.com>
2021-08-31 22:50:08 +0200

    Test

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,18 @@ const things = [
     {id: 2, name: 'bar'},
 ]
 
-const x = [...groupby(things, thing => thing.id)]
-console.log(x[0])
-console.log(x['foo'])
+const xs = groupby(things, thing => thing.id)
+for (const [k, _x] of xs) {
+    const x = [..._x]
+    console.log(k)
+    console.log(x[0])
+    console.log(x['foo'])
+}
 
-const y = [...groupby(things, thing => thing.name)]
-console.log(y[0])
-console.log(y['foo'])
+const ys = groupby(things, thing => thing.name)
+for (const [k, _y] of ys) {
+    const y = [..._y]
+    console.log(k)
+    console.log(y[0])
+    console.log(y['foo'])
+}
