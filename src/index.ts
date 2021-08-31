10bd097b87307d08812bcec9ff1c1a0180b49088
Vincent Driessen <me@nvie.com>
2021-08-31 22:45:20 +0200

    Add test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 // Test
+
+import { groupby } from 'itertools'
+
+const things = [
+    {id: 1, name: 'foo'},
+    {id: 2, name: 'bar'},
+]
+
+const x = [...groupby(things, thing => thing.id)]
+console.log(x[0])
+console.log(x['foo'])
+
+const y = [...groupby(things, thing => thing.name)]
+console.log(y[0])
+console.log(y['foo'])
