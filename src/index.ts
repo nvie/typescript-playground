b3600d3b73e6f667ae7b611eb3404ca9fe93114e
Vincent Driessen <me@nvie.com>
2021-10-30 11:04:16 +0200

    Repro for decoders-750

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
-// Test
+import { object, string } from "decoders";
+import type { Decoder } from "decoders";
 
-import { groupby } from 'itertools'
-
-const things = [
-    {id: 1, name: 'foo'},
-    {id: 2, name: 'bar'},
-]
-
-const xs = groupby(things, thing => thing.id)
-for (const [k, _x] of xs) {
-    const x = [..._x]
-    console.log(k)
-    console.log(x[0])
-    console.log(x['foo'])
+export interface Person {
+  name: string;
 }
 
-const ys = groupby(things, thing => thing.name)
-for (const [k, _y] of ys) {
-    const y = [..._y]
-    console.log(k)
-    console.log(y[0])
-    console.log(y['foo'])
-}
+export const personDecoder: Decoder<Person> = object({
+  name: string,
+});
